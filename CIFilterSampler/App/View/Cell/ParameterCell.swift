6a9dd50e776fdc3d73cf7fc6230291f4e060ce0c
ha1f <ha1f@linecorp.com>
2018-03-09 19:31:00 +0900

    tableView

---
+++ CIFilterSampler/App/View/Cell/ParameterCell.swift
@@ -0,0 +1,14 @@
+//
+//  ParameterCell.swift
+//  CIFilterSampler
+//
+//  Created by ST20591 on 2018/03/09.
+//  Copyright © 2018年 ha1f. All rights reserved.
+//
+
+import UIKit
+
+class ParameterCell: UITableViewCell {
+    @IBOutlet var keyLabel: UILabel!
+    @IBOutlet var valueLabel: UILabel!
+}
--- CIFilterSampler/App/ViewController.swift
+++ CIFilterSampler/App/ViewController.swift
@@ -7,8 +7,18 @@
 //
 
 import UIKit
+import SafariServices
 
-class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+// TODO: 処理中にぐるぐるを出す
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UITableViewDataSource {
+    @IBOutlet var tableView: UITableView! {
+        didSet {
+            tableView.allowsMultipleSelection = false
+            tableView.allowsSelection = false
+            tableView.tableFooterView = UIView()
+            tableView.dataSource = self
+        }
+    }
     
     @IBOutlet var collectionView: UICollectionView! {
         didSet {
@@ -20,10 +30,34 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         }
     }
     @IBOutlet var imageView: UIImageView!
-    @IBOutlet var descriptionView: UITextView!
+    @IBOutlet var referenceButton: UIBarButtonItem! {
+        didSet {
+            referenceButton.target = self
+            referenceButton.action = #selector(onReferenceButtonTapped)
+        }
+    }
     
-    var filterNames: [String] = []
-    lazy var ciImage = CIImage.extractOrGenerate(from: #imageLiteral(resourceName: "Lenna.png"))
+    var filterNames: [String] = [] {
+        didSet {
+            collectionView.reloadData()
+        }
+    }
+    lazy var ciImage = CIImage.extractOrGenerate(from: #imageLiteral(resourceName: "Lenna.png"))!
+    
+    var currentFilter: CIFilter?
+    var currentParameters: [String: Any?] = [:] {
+        didSet {
+            tableView.reloadData()
+        }
+    }
+    
+    @objc
+    func onReferenceButtonTapped() {
+        guard let url = currentFilter?.referenceDocumentationUrl else {
+            return
+        }
+        present(SFSafariViewController(url: url), animated: true, completion: nil)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -33,29 +67,46 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         imageView.image = #imageLiteral(resourceName: "Lenna.png")
     }
     
+    private func updateTitle() {
+        self.title = currentFilter?.displayName ?? currentFilter?.filterName
+    }
+    
     private func update(withFilterName filterName: String) {
+        currentParameters = [:]
         guard let filter = CIFilter(name: filterName) else {
+            currentFilter = nil
             return
         }
+        currentFilter = filter
+        updateTitle()
         
-        self.title = filter.displayName ?? filterName
-        
+        filter.setDefaults()
         filter.inputKeys.forEach { inputKey in
             if inputKey == kCIInputImageKey {
+                currentParameters[inputKey] = ciImage
                 filter.setValue(ciImage, forKey: kCIInputImageKey)
                 return
             }
+            if inputKey == kCIInputExtentKey {
+                currentParameters[inputKey] = CIVector(cgRect: ciImage.extent.standardized)
+                filter.setValue(CIVector(cgRect: ciImage.extent.standardized), forKey: kCIInputExtentKey)
+                return
+            }
             let information = filter.parameterInformation(forInputKey: inputKey)
-            let defaultValue = information[kCIAttributeDefault]
-            filter.setValue(defaultValue, forKey: inputKey)
+            if let defaultValue = information[kCIAttributeDefault] {
+                filter.setValue(defaultValue, forKey: inputKey)
+                currentParameters[inputKey] = defaultValue
+            } else {
+                currentParameters[inputKey] = Any?.none
+            }
         }
-        descriptionView.text = filter.referenceDocumentationUrl?.absoluteString
         
         imageView.image = filter.outputImage?.asUIImage()
+        
+        referenceButton.isEnabled = filter.referenceDocumentationUrl != nil
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        print("select", indexPath.row)
         collectionView.scrollToItem(at: indexPath, at: .centeredHorizontally, animated: true)
         
         let filterName = filterNames[indexPath.row]
@@ -71,6 +122,23 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
         cell.backgroundColor = .white
         cell.label.text = filterNames[indexPath.row]
         cell.label.textColor = .brown
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return currentParameters.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ParameterCell") as! ParameterCell
+        let item = currentParameters.dropFirst(indexPath.row).first
+        cell.keyLabel.text = item?.key
+        if let value = item?.value {
+            cell.valueLabel.text = String(describing: value)
+        } else {
+            cell.valueLabel.text = "nil"
+        }
+        
         return cell
     }
 
